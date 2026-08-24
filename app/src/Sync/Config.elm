a55ab22eecf5f0af3e0301a6b0ef71efaa5b7bd5
Felix Granum <felix@risbro.dk>
2026-08-24 22:36:56 +0200

    Added supabase url and public key

---
+++ app/src/Sync/Config.elm
@@ -0,0 +1,11 @@
+module Sync.Config exposing (anonKey, supabaseUrl)
+
+
+supabaseUrl : String
+supabaseUrl =
+    "https://vnezfrcvmwwbyazzodxu.supabase.co"
+
+
+anonKey : String
+anonKey =
+    "sb_publishable_petq_FM4tgLWba5UGtQYcQ_t5e8PeQV"
